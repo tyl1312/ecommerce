a1e1d11dcc3eacda3ff4d39d610e1a7691149e3a
NQHUng2805 <105558370+NQHUng2805@users.noreply.github.com>
2025-05-28 13:58:30 +0700

    Update Cart.js

--- backend/src/models/Cart.js
+++ backend/src/models/Cart.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
 const cartSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   items: [{
-    product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
-    quantity: Number
+    product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+    quantity: { type : Number, required: true},
+     size: { type : Schema.Types.Mixed},
+    color: { type : Schema.Types.Mixed},
   }]
 });
 
