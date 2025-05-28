93cce7ef35e952385532d134bed8cf7ab3dbe426
NQHUng2805 <105558370+NQHUng2805@users.noreply.github.com>
2025-05-28 14:24:08 +0700

    Update Order.js

--- backend/src/models/Order.js
+++ backend/src/models/Order.js
@@ -1,12 +1,25 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-const orderSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  items: [{
+const paymentMethods = {
+  values: ['card', 'cash'],
+  message: 'enum validator failed for payment Methods'
+}
+const orderSchema = new mongoose.Schema(
+  {
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  items: [
+    {
     product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
     quantity: Number
-  }],
-  createdAt: { type: Date, default: Date.now }
-});
+    }
+  ],
+  paymentMethod: { type: String, required: true, enum: paymentMethods },
+  paymentStatus: { type: String, default: 'pending' },
+  status: { type: String, default: 'pending' },
+  selectedAddress: { type: Schema.Types.Mixed, required: true },
+}
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Order', orderSchema);
