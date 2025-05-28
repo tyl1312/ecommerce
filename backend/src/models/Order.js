cbe3372eaf3df4fdbe619b5568fde65318989565
NQHUng2805 <105558370+NQHUng2805@users.noreply.github.com>
2025-05-28 14:16:14 +0700

    Add files via upload

---
+++ backend/src/models/Order.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  items: [{
+    product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+    quantity: Number
+  }],
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Order', orderSchema);
