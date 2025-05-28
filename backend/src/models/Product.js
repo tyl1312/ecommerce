a794a95f9785197a14ff84fae329b0003be99234
NQHUng2805 <105558370+NQHUng2805@users.noreply.github.com>
2025-05-28 14:09:24 +0700

    Update Product.js

--- backend/src/models/Product.js
+++ backend/src/models/Product.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  name: String,
+  name: { type : String, required: true, unique: true},
   description: String,
-  price: Number,
-  image: String,
-  // add more fields as needed
+  price: { type: Number, min:[1, 'wrong min price']},
+  images:{ type : [String], required: true},
+  colors:{ type : [Schema.Types.Mixed] },
+  sizes:{ type : [Schema.Types.Mixed]},
 });
 
 module.exports = mongoose.model('Product', productSchema);
